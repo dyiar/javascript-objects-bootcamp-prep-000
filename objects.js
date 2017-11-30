0e86aba06b41aac3f7266161a2fddf5066001f9f
Austin Blake <ummwhere@hotmail.com>
2017-11-30 05:16:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artist: "title"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({ Object (Phil Ochs) });
-  return object;
+  playlist[artistName] = songTitle;
+  return playlist;
 }
